a7142e6c39f42bfbe56b5f4fd4f1b5f6d2907714
sathwik <sathwik.sodari@gmail.com>
2023-10-18 10:14:01 +0000

    commit 1

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "tfgcptraining"
+  region = "us-east4"
+  zone = "us-east4-a"
+  
+}
+
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "mat1tr123456"
+    container_name = "tfcon"
+    access_key = "kuQ/4AliHfyV7/5xOwAz/KDEfZH7JiFJnxLvQEAL3b5u2NR/UVfrlKKS+K64jrBdSyjTFfAshY5b+ASt8TYZ8Q=="
+    key = "terraform.tfstate"
+    
+  }
+}
+
+resource "google_compute_network" "dep1" {
+  name = "vpc1-5024678"
+  routing_mode = "GLOBAL"
+  auto_create_subnetworks = false
+  
+}
+
+
