28869ebbffc08bbfaa4697179a19bd9ef493700f
sathwik-sodari <79212390+sathwik-sodari@users.noreply.github.com>
2023-10-18 10:36:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,7 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "mat1tr123456"
-    container_name = "tfcon"
-    access_key = "kuQ/4AliHfyV7/5xOwAz/KDEfZH7JiFJnxLvQEAL3b5u2NR/UVfrlKKS+K64jrBdSyjTFfAshY5b+ASt8TYZ8Q=="
-    key = "terraform.tfstate"
-    
-  }
-}
+provider "google" {
+     project = "tfgcptraining"
+     region = "us-east4"
+     zone = "us-east4-a"
 
-resource "google_compute_network" "dep1" {
-  name = "vpc1-5024678"
-  routing_mode = "GLOBAL"
-  auto_create_subnetworks = false
   
 }
-
-
